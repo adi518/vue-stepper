0b46affb6630ce44d2060a1f7d1e75b77f0a75d8
Adi Sahar <adisahar1@hotmail.com>
2018-07-18 23:59:55 +0300

    Add google-analytics

--- docs-src/src/main.js
+++ docs-src/src/main.js
@@ -6,6 +6,12 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-122603914-1'
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
