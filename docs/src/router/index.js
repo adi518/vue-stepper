de32051f3ab673ae101cd5b5a26413f9ba668410
Adi Sahar <adisahar1@hotmail.com>
2018-08-18 05:00:14 +0300

    Update routes

--- docs/src/router/index.js
+++ docs/src/router/index.js
@@ -1,21 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Docs from '@/containers/Docs'
-import HelloWorld from '@/components/HelloWorld'
-
 Vue.use(Router)
 
 export const routes = [
   {
     path: '/',
     name: 'Documentation',
-    component: Docs
+    component: () => import('@/containers/Docs')
+  },
+  {
+    path: '/',
+    name: 'Documentation 1.x',
+    component: () => import('@/containers/Docs-1.x.vue')
   }
 ]
 
 if (process.env.NODE_ENV === 'development') {
-  routes.push({ path: '/hello-world', name: 'Hello World', component: HelloWorld })
+  routes.push({
+    path: '/playground',
+    name: 'Playground',
+    component: () => import('@/components/HelloWorld.vue')
+  })
 }
 
 export default new Router({ routes, mode: 'history' })
